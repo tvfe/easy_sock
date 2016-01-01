2519ebfa43e3f066cbeb03fc0aab02b455a50acf
alsotang <alsotang@gmail.com>
2016-01-01 20:12:15 +0800

    内存泄露检测

---
+++ test/memory_leak.js
@@ -0,0 +1,40 @@
+var memwatch = require('memwatch-next')
+var EasySock = require('..')
+
+console.info = console.error = function () {}
+
+
+
+var arr = [];
+
+memwatch.gc();
+var hd = new memwatch.HeapDiff();
+
+for (var i = 0; i < 10000; i++) {
+  var socket = new EasySock({
+    ip: '127.0.0.1',
+    port: '3000'
+  })
+
+  socket.write({
+    haha: 11
+  }, function (err, data) {
+    
+  })
+
+  arr.push(socket)
+}
+
+arr.forEach(function (sock)  {
+  sock.close()
+})
+
+arr = null
+
+
+setTimeout(function () {
+  memwatch.gc();
+  var hde = hd.end();
+
+  console.log(JSON.stringify(hde, null, 2));
+}, 2000)
